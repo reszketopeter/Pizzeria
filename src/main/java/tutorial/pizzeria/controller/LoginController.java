fee7c50803ea8cd13bb5b1e936c4cdb5a3eb69b9
RPeti <reszketo.peter@gmail.com>
2025-02-02 10:28:49 +0100

    LoginController

--- src/main/java/tutorial/pizzeria/controller/LoginController.java
+++ src/main/java/tutorial/pizzeria/controller/LoginController.java
@@ -5,12 +5,10 @@ import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 import tutorial.pizzeria.dto.incoming.LoginCommand;
 import tutorial.pizzeria.service.LoginService;
 
@@ -39,5 +37,17 @@ public class LoginController {
         HttpSession session = request.getSession();
         session.invalidate();
         return new ResponseEntity<>("You have successfully logged out", HttpStatus.OK);
+    }
+
+    @GetMapping("/debug/auth")
+    public ResponseEntity<String> debugAuth(HttpSession session) {
+        log.info("Debugging authentication");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        StringBuilder response = new StringBuilder("User: " + authentication.getName() + "\nAuthorities: ");
+        authentication.getAuthorities().
+                forEach(auth -> response.append(auth.getAuthority()).append("\n"));
+        response.append(session.getAttribute("customerId"));
+
+        return ResponseEntity.ok(response.toString());
     }
 }
