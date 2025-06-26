3fb679413a2aa1b9a5f443d90c2ae950129b6b39
RPeti <reszketo.peter@gmail.com>
2025-06-26 17:19:18 +0200

    application.yaml - modification

--- src/test/java/tutorial/pizzeria/controller/OrderControllerTest.java
+++ src/test/java/tutorial/pizzeria/controller/OrderControllerTest.java
@@ -17,7 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import tutorial.pizzeria.dto.incoming.OrderCommand;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
